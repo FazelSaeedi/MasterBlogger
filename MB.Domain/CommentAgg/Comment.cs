45c10db5ffc6e96abbed6e9f51ea3205ba0ed21c
Fazel Saeedi <fazels36@gmail.com>
2021-03-20 14:17:55 +0330

    Comment Domain Implemented  - Application Layer Structure Created

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MB.Domain.ArticleAgg;
+
+namespace MB.Domain.CommentAgg
+{
+    public class Comment
+    {
+      
+        public long id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Message { get; private set; }
+        public int Status { get; private set; } // new = 0 , confirmed  =1 , Cansel = 2
+        public DateTime CreationDate { get; private set; }
+
+        public long ArticleId { get; private set; }
+        public Article Article { get; private set; }
+
+        protected Comment()
+        {
+
+        }
+        
+        public Comment(string name, string email, string message, int status, long articleId)
+        {
+            Name = name;
+            Email = email;
+            Message = message;
+            Status = Statuses.New;
+            CreationDate = DateTime.Now;
+            ArticleId = articleId;
+        }
+
+
+    }
+}
